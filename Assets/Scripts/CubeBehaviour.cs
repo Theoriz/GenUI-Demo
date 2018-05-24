7d1a23a59a2c5e54cbffbddafe9ac4e0bb7f732c
root <root@LEIA.localdomain>
2018-05-24 17:07:42 +0200

    New controllable management, no more Update method

---
+++ Assets/Scripts/CubeBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehaviour : MonoBehaviour {
+
+    public int NombreInt;
+
+    public float NombreFloat;
+
+    public string MyString;
+
+    public bool Rotate;
+
+    public int IntSlider;
+
+    public float Speed;
+    
+    public Vector3 Pos;
+
+    public Color CubeColor;
+
+    public void SetColor(Color col)
+    {
+        CubeColor = col;
+        GetComponent<Renderer>().material.color = CubeColor;
+    }
+
+    //// Use this for initialization
+    //void Start () {
+    //    GetComponent<Renderer>().material.color = CubeColor;
+    //}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Rotate)
+            transform.Rotate(new Vector3(0, 1, 0) * Speed);
+	}
+}
---
+++ Assets/Scripts/CubeBehaviourControllable.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehaviourControllable : Controllable
+{
+    public CubeBehaviour MyCubeBehaviour;
+
+    [OSCProperty]
+    public int NombreInt;
+
+    [OSCProperty]
+    public float NombreFloat;
+
+    [OSCProperty]
+    public string MyString;
+
+    [OSCProperty]
+    public bool Rotate;
+
+    [OSCProperty]
+    [Range(0, 10)]
+    public int IntSlider;
+
+    [OSCProperty]
+    [Range(0,10)]
+    public float Speed;
+
+    [OSCProperty]
+    public Vector3 Pos;
+
+    [OSCProperty]
+    public Color CubeColor;
+
+    [OSCMethod]
+    public void SetColor(Color col)
+    {
+        MyCubeBehaviour.SetColor(col);
+    }
+
+    [OSCMethod]
+    public void SetColorRed()
+    {
+        MyCubeBehaviour.SetColor(Color.red);
+    }
+
+    [OSCMethod]
+    public void SetColorWhite()
+    {
+        MyCubeBehaviour.SetColor(Color.white);
+    }
+
+    public override void Awake()
+    {
+        debug = false;
+        if (MyCubeBehaviour == null)
+        {
+            var scripts = FindObjectsOfType<CubeBehaviour>();
+            if(scripts.Length >=1 )
+                Debug.LogError("Too many CubeBehaviour script, please set the good one manually");
+            if (scripts.Length == 1)
+                MyCubeBehaviour = scripts[0];
+
+        }
+
+        if (MyCubeBehaviour == null)
+        {
+            Debug.LogWarning("Can't find CubeBehaviour script to control !");
+            return;
+        }
+        TargetScript = MyCubeBehaviour;
+        base.Awake();
+    }
+
+    public override void DataLoaded()
+    {
+        base.DataLoaded();
+        MyCubeBehaviour.SetColor(CubeColor);
+    }
+
+    //public override void OnScriptValueChanged(string name)
+    //{
+    //    NombreInt = MyCubeBehaviour.NombreInt;
+    //    NombreFloat = MyCubeBehaviour.NombreFloat;
+    //    MyString = MyCubeBehaviour.MyString;
+    //    Rotate = MyCubeBehaviour.Rotate;
+    //    IntSlider = MyCubeBehaviour.IntSlider;
+    //    Speed = MyCubeBehaviour.Speed;
+    //    Pos = MyCubeBehaviour.Pos;
+    //    CubeColor = MyCubeBehaviour.CubeColor;
+    //}
+
+    //public override void OnUiValueChanged(string name)
+    //{
+    //    MyCubeBehaviour.NombreInt = NombreInt;
+    //    MyCubeBehaviour.NombreFloat = NombreFloat;
+    //    MyCubeBehaviour.MyString = MyString;
+    //    MyCubeBehaviour.Rotate = Rotate;
+    //    MyCubeBehaviour.IntSlider = IntSlider;
+    //    MyCubeBehaviour.Speed = Speed;
+    //    MyCubeBehaviour.Pos = Pos;
+    //    MyCubeBehaviour.CubeColor = CubeColor;
+    //}
+}
